8d387ee04523331942fce9981cddecfc076e443a
Corvus <wyatta.hancock@gmail.com>
2020-09-28 11:30:05 -0600

    created an activate function that will clean up cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,10 +13,24 @@ const DATA_CACHE_NAME = "data-cache-v1";
 self.addEventListener('install', (event) => {
     event.waitUntil(
       caches.open(PRECACHE).then(cache => {
+        console.log("installing cache")
         return cache.addAll(FILES_TO_CACHE)
     })
   );
   self.skipWaiting();
 });
 
-//
+//Activate will clean up our previous caches
+self.addEventListener('activate', (event) =>{
+    console.log("beginning the search for older cached data")
+    const cacheDuo = [CACHE_NAME, DATA_CACHE_NAME];
+    event.waitUntil(caches.keys().then(keyList =>{
+        keyList.map(key => {
+            if (key !== cacheDuo){
+                console.log("cleaning up older iteration of cached data", key);
+                return caches.delete(key)
+            };
+        });
+    }));
+});
+
