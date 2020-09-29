f36be08ab1e543c059413cb8bb1394244d8911a8
Corvus <wyatta.hancock@gmail.com>
2020-09-29 12:56:21 -0600

    added html route

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
