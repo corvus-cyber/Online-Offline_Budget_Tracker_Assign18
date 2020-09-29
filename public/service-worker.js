e2c355e8f37fc3b1af3cafee76ec0903b735a0cb
Corvus <wyatta.hancock@gmail.com>
2020-09-29 15:31:39 -0600

    added cleanup

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,19 +23,19 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-// //Activate will clean up our previous caches
-// self.addEventListener('activate', (event) =>{
-//     console.log("beginning the search for older cached data")
-//     const cacheDuo = [CACHE_NAME, DATA_CACHE_NAME];
-//     event.waitUntil(caches.keys().then(keyList =>{
-//         keyList.map(key => {
-//             if (key !== cacheDuo){
-//                 console.log("cleaning up older iteration of cached data", key);
-//                 return caches.delete(key)
-//             };
-//         });
-//     }));
-// });
+//Activate will clean up our previous caches
+self.addEventListener('activate', (event) =>{
+    console.log("beginning the search for older cached data")
+    const cacheDuo = [CACHE_NAME, DATA_CACHE_NAME];
+    event.waitUntil(caches.keys().then(keyList =>{
+        keyList.map(key => {
+            if (key !== cacheDuo){
+                console.log("cleaning up older iteration of cached data", key);
+                return caches.delete(key)
+            };
+        });
+    }));
+});
 
 //Fetch 
 self.addEventListener("fetch", function(event) {
