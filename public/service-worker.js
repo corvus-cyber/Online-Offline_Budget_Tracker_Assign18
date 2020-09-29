7b867602ac559f2ec8e9dabfc2adbf6852648eea
Corvus <wyatta.hancock@gmail.com>
2020-09-29 14:49:58 -0600

    changed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -65,8 +65,11 @@ self.addEventListener("fetch", function(event) {
     event.respondWith(
         fetch(event.request).catch(function(){
             return caches.match(event.request).then(function(response) {
-                return response;
+                if (response){return response}
+                else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/")}
             })
+
         })
     );
 });
