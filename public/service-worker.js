2b31d724ffd07c0633768768d2630d391ae4de4b
Corvus <wyatta.hancock@gmail.com>
2020-09-29 14:10:51 -0600

    edited cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,9 +4,11 @@ const FILES_TO_CACHE = [
  "/styles.css",
  "/index.js",
  "/manifest.json",
+ "/icons/icon-192x192.png",
+ "/icons/icon-512x512.png"
 ];
 
-const CACHE_NAME = "static-cache-v2";
+var CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 //Install the cache
